efda36de404b230263f9754b9c141e0f343bbc3b
dimazz1337 <58118096+dimazz1337@users.noreply.github.com>
2024-07-21 18:34:21 +0300

    Небольшие изменения декоратора

--- additionals/decorators_utils.py
+++ additionals/decorators_utils.py
@@ -7,9 +7,10 @@ def print_table(func):
         if data is None:
             return
 
-        print(f"{'ID':<5} | {'Title':<20} | {'Author':<20} | {'Year':<5} | {'Status':<9}\n{'-' * 71}")
+        print(f"{'ID':<5} | {'Title':<20} | {'Author':<20} | {'Year':<5} | {'Status':<9}"
+              f"\n{'-' * 71}") # шапка таблицы
 
-        for row in data:
-            print(f"{row['id']:<5} | {row['title']:<20} | {row['author']:<20} | {row['year']:<5} | {row['status']:<9}")
+        print(*(f"{row['id']:<5} | {row['title']:<20} | {row['author']:<20} | {row['year']:<5} | {row['status']:<9}"
+              for row in data), sep='\n') # основные данные
 
     return wrapper
